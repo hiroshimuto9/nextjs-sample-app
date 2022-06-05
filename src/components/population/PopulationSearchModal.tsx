b51c5e9da96c748e1f0c6311f8d07baabfd50a6c
mutohiroshi <mutohiroshi@team-lab.com>
2022-06-05 21:16:25 +0900

    feat: 画面コンポーネントからapiの呼び出しと取得データの描画処理を実装

--- src/components/population/PopulationSearchModal.tsx
+++ src/components/population/PopulationSearchModal.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import style from "./PopulationSearchModal.module.css";
 
 type Props = {
@@ -6,7 +7,28 @@ type Props = {
   closeModal: () => void;
 };
 
+// TODO api/prefectures/index.tsにも同じ型定義があるためリファクタ
+type Response = {
+  message: null;
+  result: Prefecture[];
+};
+
+type Prefecture = {
+  prefCode: number;
+  prefName: string;
+};
+
 const PopulationSearchModal: NextPage<Props> = ({ isOpen, closeModal }) => {
+  const [prefectures, setPrefectures] = useState<Prefecture[]>([]);
+  useEffect(() => {
+    const fetchPrefectures = async () => {
+      // TODO error handling
+      const response = await fetch("/api/prefectures");
+      const prefectures: Response = await response.json();
+      setPrefectures(prefectures.result);
+    };
+    fetchPrefectures();
+  }, []);
   return (
     <>
       {isOpen && (
@@ -20,78 +42,14 @@ const PopulationSearchModal: NextPage<Props> = ({ isOpen, closeModal }) => {
             </div>
             <div className={style.modalContent}>
               <ul className={style.searchItemList}>
-                <li className={style.searchItem}>
-                  <label htmlFor="box1">
-                    <input id="box1" type="checkbox" />
-                    東京都
-                  </label>
-                </li>
-                <li className={style.searchItem}>
-                  <label htmlFor="box2">
-                    <input id="box2" type="checkbox" />
-                    神奈川県
-                  </label>
-                </li>
-                <li className={style.searchItem}>
-                  <label htmlFor="box3">
-                    <input id="box3" type="checkbox" />
-                    千葉県
-                  </label>
-                </li>
-                <li className={style.searchItem}>
-                  <label htmlFor="box4">
-                    <input id="box4" type="checkbox" />
-                    栃木県
-                  </label>
-                </li>
-                <li className={style.searchItem}>
-                  <label htmlFor="box5">
-                    <input id="box5" type="checkbox" />
-                    茨木県
-                  </label>
-                </li>
-                <li className={style.searchItem}>
-                  <label htmlFor="box6">
-                    <input id="box6" type="checkbox" />
-                    埼玉県
-                  </label>
-                </li>
-                <li className={style.searchItem}>
-                  <label htmlFor="box1">
-                    <input id="box1" type="checkbox" />
-                    東京都
-                  </label>
-                </li>
-                <li className={style.searchItem}>
-                  <label htmlFor="box2">
-                    <input id="box2" type="checkbox" />
-                    神奈川県
-                  </label>
-                </li>
-                <li className={style.searchItem}>
-                  <label htmlFor="box3">
-                    <input id="box3" type="checkbox" />
-                    千葉県
-                  </label>
-                </li>
-                <li className={style.searchItem}>
-                  <label htmlFor="box4">
-                    <input id="box4" type="checkbox" />
-                    栃木県
-                  </label>
-                </li>
-                <li className={style.searchItem}>
-                  <label htmlFor="box5">
-                    <input id="box5" type="checkbox" />
-                    茨木県
-                  </label>
-                </li>
-                <li className={style.searchItem}>
-                  <label htmlFor="box6">
-                    <input id="box6" type="checkbox" />
-                    埼玉県
-                  </label>
-                </li>
+                {prefectures.map((prefecture) => (
+                  <li className={style.searchItem} key={prefecture.prefCode}>
+                    <label htmlFor={String(prefecture.prefCode)}>
+                      <input id={String(prefecture.prefCode)} type="checkbox" />
+                      {prefecture.prefName}
+                    </label>
+                  </li>
+                ))}
               </ul>
             </div>
             <div className={style.modalFooter}>
