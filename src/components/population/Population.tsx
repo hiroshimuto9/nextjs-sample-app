637b9eec3caf2b3cfb3582bd7182d23376df52e0
mutohiroshi <mutohiroshi@team-lab.com>
2022-07-02 18:27:19 +0900

    feat: 検索処理イベント実行時、レスポンスをグラフ描画に必要な形式に変換しstateに保持する処理とグラフ描画処理を実装

--- src/components/population/Population.tsx
+++ src/components/population/Population.tsx
@@ -1,5 +1,15 @@
 import type { NextPage } from "next";
 import { useState } from "react";
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+} from "recharts";
 import style from "./Population.module.css";
 import { Layout } from "@/components/layouts/Layout";
 import PopulationSearchModal from "@/components/population/PopulationSearchModal";
@@ -27,15 +37,24 @@ type CompositionData = {
 };
 
 type Composition = {
+  label?: string;
   year: number;
   value: number;
 };
 
+type ChartData = {
+  prefectureName: string;
+  data: Composition[];
+};
+
 const PopulationPage: NextPage = () => {
   const [modalStatus, setModalStatus] = useState(false);
-  const [composition, setComposition] = useState<CompositionResult | null>(
-    null
-  );
+  const [totalPopulationList, setTotalPopulation] = useState<ChartData[]>([]);
+  const [youngPopulationList, setYoungPopulation] = useState<ChartData[]>([]);
+  const [workingAgePopulationList, setWorkingAgePopulation] = useState<
+    ChartData[]
+  >([]);
+  const [agedPopulationList, setAgedPopulation] = useState<ChartData[]>([]);
 
   const openModal = () => {
     setModalStatus(true);
@@ -46,16 +65,158 @@ const PopulationPage: NextPage = () => {
   };
 
   /** 検索処理を実行 */
-  const handleSearch = async (checkedPrefCodes: Prefecture["prefCode"][]) => {
-    const [prefCode, ...addAreaPrefCodes] = [...checkedPrefCodes];
-    const formattedAddArea = `${addAreaPrefCodes.join("_,")}_`;
+  const handleSearch = async (checkedPrefectureList: Prefecture[]) => {
+    let allTotalPopulationList: ChartData[] = [];
+    let allYoungPopulationList: ChartData[] = [];
+    let allWorkingAgePopulationList: ChartData[] = [];
+    let allAgedPopulationList: ChartData[] = [];
 
-    const response = await fetch(
-      `/api/population/composition/perYear?cityCode=-&prefCode=${prefCode}&addArea=${formattedAddArea}`
-    );
-    const composition: CompositionResponse = await response.json();
-    setComposition(composition.result);
+    for (const checkedPrefecture of checkedPrefectureList) {
+      const requestUrl = `/api/population/composition/perYear?cityCode=-&prefCode=${checkedPrefecture.prefCode}`;
+      const response = await fetch(requestUrl);
+      const composition: CompositionResponse = await response.json();
+      // 選択された都道府県の総人口を一時保存
+      if (composition.result.data[0]) {
+        const totalPopulationChartData: ChartData = {
+          prefectureName: checkedPrefecture.prefName,
+          data: composition.result.data[0].data,
+        };
+        allTotalPopulationList.push(totalPopulationChartData);
+      }
+      // 選択された都道府県の年少人口を一時保存
+      if (composition.result.data[1]) {
+        const youngPopulationChartData: ChartData = {
+          prefectureName: checkedPrefecture.prefName,
+          data: composition.result.data[1].data,
+        };
+        allYoungPopulationList.push(youngPopulationChartData);
+      }
+      // 選択された都道府県の生産年齢人口を一時保存
+      if (composition.result.data[2]) {
+        const workingAgePopulationChartData: ChartData = {
+          prefectureName: checkedPrefecture.prefName,
+          data: composition.result.data[2].data,
+        };
+        allWorkingAgePopulationList.push(workingAgePopulationChartData);
+      }
+      // 選択された都道府県の老年人口を一時保存
+      if (composition.result.data[3]) {
+        const agedPopulationChartData: ChartData = {
+          prefectureName: checkedPrefecture.prefName,
+          data: composition.result.data[3].data,
+        };
+        allAgedPopulationList.push(agedPopulationChartData);
+      }
+    }
+    setTotalPopulation(allTotalPopulationList);
+    setYoungPopulation(allYoungPopulationList);
+    setWorkingAgePopulation(allWorkingAgePopulationList);
+    setAgedPopulation(allAgedPopulationList);
   };
+
+  const totalPopulationLineChart = (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart
+        width={600}
+        height={300}
+        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+      >
+        {totalPopulationList.map((totalPopulation) => (
+          <Line
+            dataKey="value"
+            data={totalPopulation.data}
+            name={totalPopulation.prefectureName}
+            key={totalPopulation.prefectureName}
+            type="monotone"
+            stroke="#8884d8"
+          />
+        ))}
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="year" />
+        <YAxis dataKey="value" />
+        <Tooltip />
+        <Legend />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+
+  const youngPopulationLineChart = (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart
+        width={600}
+        height={300}
+        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+      >
+        {youngPopulationList.map((youngPopulation) => (
+          <Line
+            dataKey="value"
+            data={youngPopulation.data}
+            name={youngPopulation.prefectureName}
+            key={youngPopulation.prefectureName}
+            type="monotone"
+            stroke="#8884d8"
+          />
+        ))}
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="year" />
+        <YAxis dataKey="value" />
+        <Tooltip />
+        <Legend />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+
+  const workingAgePopulationLineChart = (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart
+        width={600}
+        height={300}
+        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+      >
+        {workingAgePopulationList.map((workingAgePopulation) => (
+          <Line
+            dataKey="value"
+            data={workingAgePopulation.data}
+            name={workingAgePopulation.prefectureName}
+            key={workingAgePopulation.prefectureName}
+            type="monotone"
+            stroke="#8884d8"
+          />
+        ))}
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="year" />
+        <YAxis dataKey="value" />
+        <Tooltip />
+        <Legend />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+
+  const agedPopulationLineChart = (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart
+        width={600}
+        height={300}
+        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+      >
+        {agedPopulationList.map((agedPopulation) => (
+          <Line
+            dataKey="value"
+            data={agedPopulation.data}
+            name={agedPopulation.prefectureName}
+            key={agedPopulation.prefectureName}
+            type="monotone"
+            stroke="#8884d8"
+          />
+        ))}
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="year" />
+        <YAxis dataKey="value" />
+        <Tooltip />
+        <Legend />
+      </LineChart>
+    </ResponsiveContainer>
+  );
 
   return (
     <div>
@@ -83,15 +244,27 @@ const PopulationPage: NextPage = () => {
           <h1 className={style.mainCategoryTitle}>人口構成</h1>
           <div className={style.subCategoryContentsArea}>
             <div className={style.subCategoryContentWrap}>
-              <h2 className={style.subCategoryTitle}>人口構成</h2>
+              <h2 className={style.subCategoryTitle}>総人口</h2>
               <div className={style.subCategoryContent}>
-                <p>人口構成のグラフエリア</p>
+                {totalPopulationLineChart}
               </div>
             </div>
             <div className={style.subCategoryContentWrap}>
-              <h2 className={style.subCategoryTitle}>人口ピラミッド</h2>
+              <h2 className={style.subCategoryTitle}>年少人口</h2>
               <div className={style.subCategoryContent}>
-                <p>人口ピラミッドのグラフエリア</p>
+                {youngPopulationLineChart}
+              </div>
+            </div>
+            <div className={style.subCategoryContentWrap}>
+              <h2 className={style.subCategoryTitle}>生産年齢人口</h2>
+              <div className={style.subCategoryContent}>
+                {workingAgePopulationLineChart}
+              </div>
+            </div>
+            <div className={style.subCategoryContentWrap}>
+              <h2 className={style.subCategoryTitle}>老年人口</h2>
+              <div className={style.subCategoryContent}>
+                {agedPopulationLineChart}
               </div>
             </div>
           </div>
